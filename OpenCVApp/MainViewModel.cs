5be54b439281307ed7b05bd070950b64b715e280
Mikko Kokkonen <mikko.kokkonen@olen.to>
2017-09-26 21:02:07 +0300

    Initial commit

---
+++ OpenCVApp/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCVApp
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _message;
+
+        public MainViewModel()
+        {
+            _message = "Welcome to Open CV Application";
+        }
+
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                _message += "\n";
+                _message += value;
+                OnPropertyChanged("Message");
+            }
+        }
+
+        private void OnPropertyChanged(string message)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(message));
+        }
+    }
+}
---
+++ OpenCVApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace OpenCVApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel _mainViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mainViewModel = new MainViewModel();
+            DataContext = _mainViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Message = "Nappia painettu";
+        }
+    }
+}
