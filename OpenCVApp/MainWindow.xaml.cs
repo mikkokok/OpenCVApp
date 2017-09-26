e659068af50de5a5577e60951579b1a6b21eb1f1
Mikko Kokkonen <mikko.kokkonen@olen.to>
2017-09-26 21:27:43 +0300

    Small changes

--- OpenCVApp/MainWindow.xaml.cs
+++ OpenCVApp/MainWindow.xaml.cs
@@ -1,27 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Emgu.CV;
-using Emgu.CV.CvEnum;
-using Emgu.CV.Structure;
+﻿using System.Windows;
 
 namespace OpenCVApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         private readonly MainViewModel _mainViewModel;
         public MainWindow()
@@ -31,9 +15,21 @@ namespace OpenCVApp
             DataContext = _mainViewModel;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            _mainViewModel.Message = "Nappia painettu";
+            _mainViewModel.Message = "Search Nappia painettu";
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Message = "Cancel Nappia painettu";
+        }
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Message = "Cancel Nappia painettu";
+        }
+        private void SelectFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Message = "Cancel Nappia painettu";
         }
     }
 }
